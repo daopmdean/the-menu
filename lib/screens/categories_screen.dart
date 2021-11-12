ab924b460fa057a7af4137ce3b635d0e83da7e77
Dao Pham <daopm.dean@gmail.com>
2021-11-12 21:56:39 +0700

    style & theme

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -11,6 +11,7 @@ class CategoriesScreen extends StatelessWidget {
         title: const Text('DeliMeal'),
       ),
       body: GridView(
+        padding: const EdgeInsets.all(20),
         children: DUMMY_CATEGORIES
             .map(
               (catData) => CategoryItem(
