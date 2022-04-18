f1e205dd904a8e2735ecc6d4512b31f9a0fdb1b4
Dao Pham <daopm.dean@gmail.com>
2022-04-18 14:59:25 +0700

    filters switches

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -1,8 +1,56 @@
 import 'package:flutter/material.dart';
 import 'package:the_menu/widgets/main_drawer.dart';
 
-class FiltersScreen extends StatelessWidget {
+class FiltersScreen extends StatefulWidget {
   static const routeName = '/filters';
+
+  @override
+  _FiltersScreenState createState() => _FiltersScreenState();
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
+  bool _glutenFree = false;
+  bool _vegan = false;
+  bool _vegetarian = false;
+  bool _lactoseFree = false;
+
+  Widget _buildSwitch(
+    String title,
+    String subTitle,
+    bool value,
+    Function onChanged,
+  ) {
+    return SwitchListTile(
+      title: Text(title),
+      subtitle: Text(subTitle),
+      value: value,
+      onChanged: onChanged,
+    );
+  }
+
+  void _updateGlutenFree(bool value) {
+    setState(() {
+      _glutenFree = value;
+    });
+  }
+
+  void _updateVegan(bool value) {
+    setState(() {
+      _vegan = value;
+    });
+  }
+
+  void _updateVegetarian(bool value) {
+    setState(() {
+      _vegetarian = value;
+    });
+  }
+
+  void _updateLactoseFree(bool value) {
+    setState(() {
+      _lactoseFree = value;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,10 +59,43 @@ class FiltersScreen extends StatelessWidget {
         title: Text('Filters'),
       ),
       drawer: MainDrawer(),
-      body: Container(
-        child: Center(
-          child: Text('Filters'),
-        ),
+      body: Column(
+        children: [
+          Container(
+            child: Center(
+              child: Text('Filters'),
+            ),
+          ),
+          Expanded(
+              child: ListView(
+            children: [
+              _buildSwitch(
+                'Gluten-free',
+                'Only gluten-free meals',
+                _glutenFree,
+                _updateGlutenFree,
+              ),
+              _buildSwitch(
+                'Vegan',
+                'Only Vegan meals',
+                _vegan,
+                _updateVegan,
+              ),
+              _buildSwitch(
+                'Vegetarian',
+                'Only Vegetarian meals',
+                _vegetarian,
+                _updateVegetarian,
+              ),
+              _buildSwitch(
+                'Gluten-free',
+                'Only gluten-free meals',
+                _lactoseFree,
+                _updateLactoseFree,
+              ),
+            ],
+          ))
+        ],
       ),
     );
   }
